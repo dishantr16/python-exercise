e44e4db276364b144b3dbb5d2ec9aa2de66f6764
Dishant Rathi <dishant16rathi@gmail.com>
2021-04-29 17:23:16 +0530

    date_time & Area_of_circle

---
+++ area_of_circle.py
@@ -0,0 +1,5 @@
+radius = float(input("Enter radius: "))
+pi = 3.14
+Area = pi * radius * radius
+
+print(f"Area of Circle is: {Area}")
---
+++ date_time.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+
+date = datetime.now()
+print(date.strftime("%Y-%m-%d %H:%M:%S"))
