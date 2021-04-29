e803607e28c64947cde366eadcd5abcc8698073d
Dishant Rathi <dishant16rathi@gmail.com>
2021-04-29 17:36:16 +0530

    first_last

---
+++ first_last_list.py
@@ -0,0 +1,6 @@
+"""Write a Python program to display the first and last colors from the following list."""
+
+
+color_list = ['red','green','blue','pink','black']
+
+print(color_list[0], color_list[-1])
